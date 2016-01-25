95e5a9604135685604472d5160bb734ce7589439
Conor Campbell <Conorc1000@gmail.com>
2016-01-25 10:01:32 +0000

    Update test.js

--- test.js
+++ test.js
@@ -27,6 +27,29 @@ function checkTheElementsOnTheDom() {
 
 }
 
+// Write the test for this function below
+function divideByTwo(n){
+  var doubled = n/2;
+  return doubled;
+}
+
+// Write the test for this function below
+function isPrime(n){
+  // If n is less than 2 or not an integer then by definition cannot be prime.
+   if (n < 2) {return false}
+   if (n != Math.round(n)) {return false}
+
+   // Now assume that n is prime, we will try to prove that it is not.
+   var isPrime = true;
+
+   // Now check every whole number from 2 to the square root of n. If any of these divides n exactly, n cannot be prime.
+   for (var i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i == 0) {isPrime = false}
+   }
+
+   // Finally return whether n is prime or not.
+   return isPrime;
+}
 
 
 // TESTS
@@ -62,4 +85,19 @@ test("Is the title on the DOM", function(assert) {
 test("Are all the elements on the DOM", function(assert) {
   var actual = checkTheElementsOnTheDom();
   assert.equal(actual, true, "Passed!");
+});
+
+test("Are all the elements on the DOM", function(assert) {
+  var actual = checkTheElementsOnTheDom();
+  assert.equal(actual, true, "Passed!");
+});
+
+test("test the fucntion divideByTwo", function(assert) {
+  // write the test here
+
+});
+
+test("test the fucntion isPrime", function(assert) {
+  // write the test here
+  
 });
